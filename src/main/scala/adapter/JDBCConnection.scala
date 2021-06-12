a865e48006b35716a6dc9c10dc6082ab03ad65b8
giangstrider <strider.giang@live.com>
2021-06-12 12:32:40 +1000

    Exception handling for JDBC access with Try/Success/Failure

--- src/main/scala/adapter/JDBCConnection.scala
+++ src/main/scala/adapter/JDBCConnection.scala
@@ -6,7 +6,10 @@ import java.sql.Connection
 import java.util.Properties
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import anorm.{BatchSql, NamedParameter, RowParser, SQL, ToParameterList}
-import configuration.Pinecone.{pineconeConf, connectionConf}
+import configuration.Pinecone.{connectionConf, pineconeConf}
+import exception.PineconeExceptionHandler.exceptionStop
+
+import scala.util.{Failure, Success, Try}
 
 
 object JDBCConnection {
@@ -35,37 +38,41 @@ class JDBCConnection(val config: Map[String, String]) {
 			throw new Exception(s"Database $databaseName not supported by Pinecone yet"))
 	}
 	private var initializedConnection = None: Option[Connection]
-	
+
 	private implicit def getConnection: Connection = {
 		initializedConnection match {
 			case Some(connection) => connection
-			case None =>
-				val connection = initConnection
-				initializedConnection = Some(connection)
-				connection
+			case None => initConnection match {
+					case Success(c) => initializedConnection = Some(c); c
+					case Failure(exception) => exceptionStop(exception)
+				}
 		}
 	}
 
-	private def initConnection: Connection = {
+	private def initConnection: Try[Connection] = {
 		val properties = new Properties
 		config.map(c => properties.put(c._1, c._2))
 		properties.put("driverClassName", getDriver)
-		val hikariConfig = new HikariConfig(properties)
-		val dataSource = new HikariDataSource(hikariConfig)
-		dataSource.getConnection
-	}
-
-	def executeQuery[R](query: String, parser: RowParser[R]): List[R] = {
-		val sqlResult = SQL(query).executeQuery
-		sqlResult.statementWarning match {
-			case Some(warning) =>
-				warning.printStackTrace()
-				sqlResult.as(parser.*)
-			case _ => sqlResult.as(parser.*)
+		Try {
+			val hikariConfig = new HikariConfig(properties)
+			val dataSource = new HikariDataSource(hikariConfig)
+			dataSource.getConnection
 		}
 	}
 
-	def executeInsert[R](tableName: String, macroParam: ToParameterList[R], data: List[R])	= {
+	def executeQuery[R](query: String, parser: RowParser[R]): Try[List[R]] = {
+		Try {
+			val sqlResult = SQL(query).executeQuery
+			sqlResult.statementWarning match {
+				case Some(warning) =>
+					warning.printStackTrace()
+					sqlResult.as(parser.*)
+				case _ => sqlResult.as(parser.*)
+			}
+		}
+	}
+
+	def executeInsert[R](tableName: String, macroParam: ToParameterList[R], data: List[R]): Try[Int] = {
 		val dataParams = data.map(macroParam).zipWithIndex.map{
 			case(row, i) => row.map(r => NamedParameter(s"${SnakeCase(r.name)}_$i", r.value))
 		}
@@ -73,10 +80,13 @@ class JDBCConnection(val config: Map[String, String]) {
 		val placeholders = dataParams.map(row => s"(${row.map(n => s"{${n.name}}").mkString(",")})").mkString(",")
 
 		val placeholderQuery = s"INSERT INTO $tableName($columns) VALUES $placeholders"
-		SQL(placeholderQuery).on(dataParams.flatten: _*).executeUpdate
+
+		Try(SQL(placeholderQuery).on(dataParams.flatten: _*).executeUpdate)
+
 	}
 
-	def executeUpdateById[R](tableName: String, macroParam: ToParameterList[R], data: List[R], compositeKey: Set[String])	= {
+	def executeUpdateById[R](tableName: String, macroParam: ToParameterList[R],
+	                         data: List[R], compositeKey: Set[String]): Try[Array[Int]] = {
 		val dataParams = data.map(macroParam).map(row => row.map(r => NamedParameter(SnakeCase(r.name), r.value)))
 
 		val columns = dataParams.head.map(_.name)
@@ -84,6 +94,6 @@ class JDBCConnection(val config: Map[String, String]) {
 		val compositeKeyColumns = compositeKey.map(column => s"$column = {$column}").mkString(" AND ")
 
 		val placeholderQuery = s"UPDATE $tableName SET $updatingColumns WHERE $compositeKeyColumns"
-		BatchSql(placeholderQuery, dataParams.head, dataParams.tail:_*).execute
+		Try(BatchSql(placeholderQuery, dataParams.head, dataParams.tail:_*).execute)
 	}
 }
---
+++ src/main/scala/exception/PineconeExceptionHandler.scala
@@ -0,0 +1,5 @@
+package exception
+
+class Pinecone {
+
+}
