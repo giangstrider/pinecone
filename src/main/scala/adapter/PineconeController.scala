b277698d237f31944b136945c3c3c56a0cc2982c
giangstrider <strider.giang@live.com>
2021-07-27 20:12:58 +1000

    Add Planner to transform Queries to executable format

--- src/main/scala/adapter/PineconeController.scala
+++ src/main/scala/adapter/PineconeController.scala
@@ -1,16 +1,27 @@
 package adapter
 
-import anorm.Macro
+import anorm.{Macro, RowParser, SqlParser}
+import anorm.SqlParser._
+import anorm.Column.{ columnToArray }
 import anorm.Macro.ColumnNaming.SnakeCase
+import anorm.~
 import configuration.Pinecone.pineconeConf
 import exception.PineconeExceptionHandler.exceptionStop
 import reconciliation.QueryStage.PrepareQuery
+
 import scala.util.{Failure, Success}
 
 
 object ReconController {
-	private val reconControlParser = Macro.namedParser[PrepareQuery](SnakeCase)
-	private val connection = GeneralConnection.getJdbcConnection(pineconeConf.database.connectionName)
+//	private val reconControlParser = Macro.namedParser[PrepareQuery](SnakeCase)
+	private val reconControlParser: RowParser[PrepareQuery] =
+		str("QUERY_KEY") ~ str("SOURCE_NAME") ~ str("SOURCE_QUERY") ~ str("TARGET_NAME") ~ get[String]("TARGET_QUERY") ~ double("ACCEPTED_DEVIATION") map {
+			case a ~ b ~ c ~ d  ~e ~ f  => PrepareQuery(a, b, c, d, e, f, List("TradingDate"))
+		}
+
+	private val p = str("name") ~ int("population") map { case n ~ p  => (n, p) }
+
+	private val connection = GeneralConnection.getSnowflakeConnection(pineconeConf.database.connectionName)
 	private val fullyQualifiedTableName =
 		s"${pineconeConf.database.databaseName}.${pineconeConf.database.schemaName}.${pineconeConf.database.queriesTableName}"
 	private val reconCompositeKey: Set[String] = Set("query_key", "source_name", "target_name")
